92736574e89265e568e7eef02a990da64a4b0d13
Pranav Kotwal <pranavkotwal09@gmail.com>
2026-03-17 13:41:48 +0530

    Create Butterfly.py

---
+++ Butterfly.py
@@ -0,0 +1,35 @@
+n = 5
+
+# upper
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        if j == 1 or j == i:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    
+    print(" "*(2*(n-i)), end="")
+    
+    for j in range(1, i+1):
+        if j == 1 or j == i:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
+
+# lower
+for i in range(n, 0, -1):
+    for j in range(1, i+1):
+        if j == 1 or j == i:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    
+    print(" "*(2*(n-i)), end="")
+    
+    for j in range(1, i+1):
+        if j == 1 or j == i:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
